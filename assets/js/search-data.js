f5fa2cc20bc0f89d55faaf772351ded4e2aa4b20
FelixFrohnertDB <f.frohnert@liacs.leidenuniv.nl>
2025-04-14 07:46:07 +0000

    Deploying to gh-pages from @ FelixFrohnertDB/felixfrohnertdb.github.io@5eed342bfe3e1149f397e34773ff4bbf673b031d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -415,9 +415,9 @@ ninja.data = [{
           description: "Quantum Climate Challenge Berlin",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-",
-          title: '',
-          description: "",
+            },},{id: "projects-hackathon",
+          title: 'Hackathon',
+          description: "LOQCathon Paris",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{
