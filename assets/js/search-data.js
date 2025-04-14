dcb95a392a9939497114d9022eb55fbd0825064e
FelixFrohnertDB <f.frohnert@liacs.leidenuniv.nl>
2025-04-14 08:29:50 +0000

    Deploying to gh-pages from @ FelixFrohnertDB/felixfrohnertdb.github.io@f55d7e8e83b25701c66371b33eef83b5dda6edec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -400,14 +400,14 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-",
-          title: '',
-          description: "",
+          section: "News",},{id: "projects-consulting",
+          title: 'Consulting',
+          description: "Dutch Ministry of Finance and the auditability of quantum algorithms",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-",
-          title: '',
-          description: "",
+            },},{id: "projects-podcast",
+          title: 'Podcast',
+          description: "Discussing my work on predicting emergent research directions",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-hackathon",
