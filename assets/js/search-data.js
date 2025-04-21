f12ceca9cb552dff609967bd255cc37a834ce612
FelixFrohnertDB <f.frohnert@liacs.leidenuniv.nl>
2025-04-21 13:19:28 +0000

    Deploying to gh-pages from @ FelixFrohnertDB/felixfrohnertdb.github.io@f36dc14f49309a28774979ea27f95c6521c6334b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -420,6 +420,11 @@ ninja.data = [{
           description: "LOQCathon Paris",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-mentorship",
+          title: 'Mentorship',
+          description: "Supporting quantum master&#39;s students in their first-year projects",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
